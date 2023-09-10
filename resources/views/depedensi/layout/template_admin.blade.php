6605c2d10bdc6098ebfa2febd10ade12372eac8c
kusin <aryajayaalamsyah@gmail.com>
2023-09-10 17:42:17 +0700

    -

--- resources/views/depedensi/layout/template_admin.blade.php
+++ resources/views/depedensi/layout/template_admin.blade.php
@@ -8,91 +8,87 @@
 <link rel="stylesheet" href="{{ asset('sweetalert/sweetalert2.min.css') }}">
 <script src="{{ asset('sweetalert/sweetalert2.min.js') }}"></script>
 
-  <!-- Google Font: Source Sans Pro -->
-  {{-- <link rel="stylesheet" href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> ') }}--}}
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- Ionicons -->
-  {{-- <link rel="stylesheet" href="{{ asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> ') }}--}}
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/jqvmap/jqvmap.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/dist/css/adminlte.min.css') }}">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/daterangepicker/daterangepicker.css') }}">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{ asset('adminLTE/plugins/summernote/summernote-bs4.min.css') }}">
-  <!-- jQuery -->
-  <script src="{{ asset('adminLTE/plugins/jquery/jquery.min.js') }}"></script>
-  <!-- jQuery UI 1.11.4 -->
-  <script src="{{ asset('adminLTE/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
-  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-  <script>
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/fontawesome-free/css/all.min.css') }}">
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<!-- JQVMap -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/jqvmap/jqvmap.min.css') }}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ asset('adminLTE/dist/css/adminlte.min.css') }}">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/daterangepicker/daterangepicker.css') }}">
+<!-- summernote -->
+<link rel="stylesheet" href="{{ asset('adminLTE/plugins/summernote/summernote-bs4.min.css') }}">
+<!-- jQuery -->
+<script src="{{ asset('adminLTE/plugins/jquery/jquery.min.js') }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset('adminLTE/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
     $.widget.bridge('uibutton', $.ui.button)
-  </script>
-  <!-- Bootstrap 4 -->
-  <script src="{{ asset('adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <!-- ChartJS -->
-  <script src="{{ asset('adminLTE/plugins/chart.js/Chart.min.js') }}"></script>
-  <!-- Sparkline -->
-  <script src="{{ asset('adminLTE/plugins/sparklines/sparkline.js') }}"></script>
-  <!-- JQVMap -->
-  <script src="{{ asset('adminLTE/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-  <script src="{{ asset('adminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
-  <!-- jQuery Knob Chart -->
-  <script src="{{ asset('adminLTE/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
-  <!-- daterangepicker -->
-  <script src="{{ asset('adminLTE/plugins/moment/moment.min.js') }}"></script>
-  <script src="{{ asset('adminLTE/plugins/daterangepicker/daterangepicker.js') }}"></script>
-  <!-- Tempusdominus Bootstrap 4 -->
-  <script src="{{ asset('adminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-  <!-- Summernote -->
-  <script src="{{ asset('adminLTE/plugins/summernote/summernote-bs4.min.js') }}"></script>
-  <!-- overlayScrollbars -->
-  <script src="{{ asset('adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-  <!-- AdminLTE App -->
-  <script src="{{ asset('adminLTE/dist/js/adminlte.js') }}"></script>
-  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="{{ asset('adminLTE/dist/js/pages/dashboard.js') }}"></script>
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- ChartJS -->
+<script src="{{ asset('adminLTE/plugins/chart.js/Chart.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset('adminLTE/plugins/sparklines/sparkline.js') }}"></script>
+<!-- JQVMap -->
+<script src="{{ asset('adminLTE/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('adminLTE/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ asset('adminLTE/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+<!-- daterangepicker -->
+<script src="{{ asset('adminLTE/plugins/moment/moment.min.js') }}"></script>
+<script src="{{ asset('adminLTE/plugins/daterangepicker/daterangepicker.js') }}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{ asset('adminLTE/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+<!-- Summernote -->
+<script src="{{ asset('adminLTE/plugins/summernote/summernote-bs4.min.js') }}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset('adminLTE/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('adminLTE/dist/js/adminlte.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ asset('adminLTE/dist/js/pages/dashboard.js') }}"></script>
 
-  <script>
-
-$(document).ready(function () {
-    $("#logout-admin").click(function () {
-        const jwtToken = localStorage.getItem("jwtToken");
-        $.ajax({
-            url: `/api/auth/logout`,
-            type: "post",
-            headers: {
-                Authorization: `Bearer ${jwtToken}`,
-            },
-            dataType: "json",
-            success: (response) => {
-              localStorage.removeItem('jwtToken');
-                window.location.href = `/`;
-            },
-            error: (xhr) => {
-                // Menampilkan pesan error AJAX
-                let errors;
-                if (xhr.responseJSON.errors) {
-                    errors = this.objectToString(xhr.responseJSON.errors);
-                } else {
-                    errors = this.objectToString(xhr.responseJSON);
-                }
-                this.showErrorMessage(errors).then(() => {
-                    if (xhr.status == 401) {
-                        window.location.href = "/";
+<script>
+    $(document).ready(function() {
+        $("#logout-admin").click(function() {
+            const jwtToken = localStorage.getItem("jwtToken");
+            $.ajax({
+                url: `/api/auth/logout`,
+                type: "post",
+                headers: {
+                    Authorization: `Bearer ${jwtToken}`,
+                },
+                dataType: "json",
+                success: (response) => {
+                    localStorage.removeItem('jwtToken');
+                    window.location.href = `/`;
+                },
+                error: (xhr) => {
+                    // Menampilkan pesan error AJAX
+                    let errors;
+                    if (xhr.responseJSON.errors) {
+                        errors = this.objectToString(xhr.responseJSON.errors);
+                    } else {
+                        errors = this.objectToString(xhr.responseJSON);
                     }
-                });
-            },
+                    this.showErrorMessage(errors).then(() => {
+                        if (xhr.status == 401) {
+                            window.location.href = "/";
+                        }
+                    });
+                },
+            });
         });
     });
-});
-
-  </script>
+</script>
--- resources/views/layout/template_admin.blade.php
+++ resources/views/layout/template_admin.blade.php
@@ -125,18 +125,23 @@
 
 		<!-- Main Sidebar Container -->
 		<aside class="main-sidebar sidebar-dark-primary elevation-4">
-			<!-- sidebar logo -->
-			<a href="{{ url('') }}" class="brand-link mb-4">
-				<img src="{{ asset('images/smk3gu0ke.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-				<span class="brand-text font-weight-light">SMK Triguna Utama</span>
-			</a>
-			<!-- ./sidebar logo -->
 
 			<!-- Sidebar -->
 			<div class="sidebar">
 
+				<!-- sidebar logo -->
+				<div class="user-panel mt-3 mb-3 pb-3 d-flex">
+					<div class="image">
+						<img src="{{ asset('images/smk3gu0ke.png') }}" alt="AdminLTE Logo" class="img-circle elevation-2" style="opacity: .8">
+					</div>
+					<div class="info">
+						<a href="{{ url('') }}" class="d-block">SMK Triguna Utama</a>
+					</div>
+				</div>
+				<!-- sidebar logo -->
+
 				<!-- Sidebar Menu -->
-				<nav class="mt-0">
+				<nav class="mt-2">
 					<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 						<!-- li-main-dashboard -->
 						<li class="nav-item">
@@ -255,6 +260,7 @@
 			@yield('mainContent')
 		</div>
 		<!-- /.content-wrapper -->
+
 		<footer class="main-footer">
 			<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
 			All rights reserved.
